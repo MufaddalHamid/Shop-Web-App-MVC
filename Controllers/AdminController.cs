bb5b25fe5dabd6f1c4f5189910e2f3d7cc43e608
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2024-10-09 01:01:58 +0530

    framework changes

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tool_Shop_Web_App.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View("AdminDashboard");
+        }
+    }
+}
